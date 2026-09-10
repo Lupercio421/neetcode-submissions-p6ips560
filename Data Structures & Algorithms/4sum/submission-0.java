b9cc46bb831439c83cfd1ac45220988b6747b1c4
Daniel Lupercio <41970268+Lupercio421@users.noreply.github.com>
2026-09-09 22:20:08 -0400

    Add: 4sum - submission-0

---
+++ Data Structures & Algorithms/4sum/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        // nums = [3,2,3,-3,1,0]
+        // sortedNums = [-3,0,1,2,3,3]
+        // Does it help to fix the first two numbers in nums?
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                int left = j + 1, right = n - 1;
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                        while (left < right && nums[right] == nums[right + 1]) right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
